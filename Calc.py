9055e74303ba9920db2e8f6295e52628f4ad3100
Androsz11 <73430696+Androsz11@users.noreply.github.com>
2020-10-29 17:17:49 +0530

    Add files via upload

---
+++ Calc.py
@@ -0,0 +1,30 @@
+print("Calculator");
+print
+print("Scripted by Andros");
+
+a=int(input("Enter 1st number:"))
+b=int(input("Enter second number"))
+
+print("Operations are +,-,*,/");
+
+c=input("Enter any operation");
+
+if c=="+":
+    print(a,"-",b,"=",a+b)
+    print("Thanks for using this Calculator..")
+    
+elif c=="-":
+    print(a,"-",b,"=",a-b)
+    print("Thanks for using this Calculator..")
+
+elif c=="*":
+    print(a,"*",b,"=",a*b)
+    print("Thanks for using this Calculator..")
+
+elif c=="/":
+    print(a,"/",b,"=",a/b)
+    print("Thanks for using this Calculator..")
+
+else:
+    print("Wrong input...")
+    print("Please try again.")
